0c08f5fa25b2065713ce304dea68e8c3eea4116b
Knut Lorvik <lorviken@gmail.com>
2015-04-13 17:28:17 +0200

    Update amqp

--- src/main/java/gainmaster/service/user/web/amqp/configuration/UserRabbitServerConfiguration.java
+++ src/main/java/gainmaster/service/user/web/amqp/configuration/UserRabbitServerConfiguration.java
@@ -3,6 +3,7 @@ package gainmaster.service.user.web.amqp.configuration;
 import gainmaster.service.user.web.amqp.gateway.UserRabbitGateway;
 import gainmaster.service.user.web.amqp.handler.UserReplyHandler;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
@@ -20,7 +21,6 @@ import org.springframework.context.annotation.Configuration;
 public class UserRabbitServerConfiguration extends RabbitServerConfiguration{
 
     protected final static String USER_EXCHANGE_NAME    = "gainmaster.user.exchange";
-    protected final static String USER_QUEUE_NAME       = "gainmaster.user.queue";
     protected final static String USER_REPLY_QUEUE_NAME = "gainmaster.user.reply.queue";
 
     @Bean
@@ -50,10 +50,7 @@ public class UserRabbitServerConfiguration extends RabbitServerConfiguration{
     public MessageConverter jsonMessageConverter() { return new JsonMessageConverter();}
 
     @Bean
-    public DirectExchange userDataExchange() { return new DirectExchange(USER_EXCHANGE_NAME);}
-
-    @Bean
-    public Queue userQueue() { return new Queue(USER_QUEUE_NAME);}
+    public FanoutExchange userDataExchange() { return new FanoutExchange(USER_EXCHANGE_NAME);}
 
     @Bean
     public Queue replyUserQueue(){ return new Queue(USER_REPLY_QUEUE_NAME);}
@@ -61,7 +58,6 @@ public class UserRabbitServerConfiguration extends RabbitServerConfiguration{
     private void configureTemplate(RabbitTemplate template){
         template.setMessageConverter(jsonMessageConverter());
         template.setExchange(USER_EXCHANGE_NAME);
-        template.setQueue(USER_QUEUE_NAME);
         template.setReplyQueue(replyUserQueue());
         template.setReplyTimeout(60000);
     }
