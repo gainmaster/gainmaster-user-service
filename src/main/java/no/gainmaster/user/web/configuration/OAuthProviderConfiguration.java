28cccdd9bffcb66be5f1b5359cb6b1b7fe42162d
Tony Hesjevik <tony@hesjevik.no>
2015-04-14 16:13:24 +0200

    Add token lookup

--- src/main/java/no/gainmaster/user/web/configuration/OAuthProviderConfiguration.java
+++ src/main/java/no/gainmaster/user/web/configuration/OAuthProviderConfiguration.java
@@ -1,10 +1,13 @@
 package no.gainmaster.user.web.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 @Configuration
 @EnableResourceServer
@@ -12,7 +15,6 @@ public class OAuthProviderConfiguration extends ResourceServerConfigurerAdapter 
 
     @Override
     public void configure(ResourceServerSecurityConfigurer oauthProvider) throws Exception {
-        //oauthProvider.
         oauthProvider.resourceId("gainmaster-user-service");
     }
 
@@ -20,4 +22,14 @@ public class OAuthProviderConfiguration extends ResourceServerConfigurerAdapter 
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll();
     }
+
+    @Bean
+    public ResourceServerTokenServices resourceTokenService() {
+        RemoteTokenServices tokenServices = new RemoteTokenServices();
+        tokenServices.setClientId("client");
+        tokenServices.setClientSecret("secret");
+        tokenServices.setCheckTokenEndpointUrl("http://api.hesjevik.im/oauth/check_token");
+        return tokenServices;
+    }
+
 }
