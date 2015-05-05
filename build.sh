c28dc01de93ca67d5e728ee89f9fa9880d29696b
Knut Lorvik <lorviken@gmail.com>
2015-05-05 13:48:15 +0200

    Modify buildscript

--- build.sh
+++ build.sh
@@ -1,41 +1,39 @@
 #!/usr/bin/env bash
 
-set -x              # Print command traces before executing command
 trap 'exit 1' ERR   # Exit script with error if command fails
-
-cd $(dirname "${BASH_SOURCE[0]}")
 
 if [[ -z $(which docker) ]]; then
     echo "Missing docker client which is required for building, testing and pushing."
     exit 2
 fi
 
+declare PROJECT_DIRECTORY=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+declare DOCKER_IMAGE_NAME="gainmaster/gainmaster-user-service"
 
-declare IMAGE_NAME="gainmaster/gainmaster-user-service"
-
+cd $PROJECT_DIRECTORY
 
 function build {
-        docker build -t "${IMAGE_NAME}" .
+        docker build -t "${DOCKER_IMAGE_NAME}" .
 }
 
 
 function test {
-    docker history "${IMAGE_NAME}" 2> /dev/null
+    docker history "${DOCKER_IMAGE_NAME}" 2> /dev/null
 
     if [ $? -eq 1 ]; then
-        echo "Cant test ${IMAGE_NAME}, the image does not exist."
+        echo "Cant test ${DOCKER_IMAGE_NAME}, the image does not exist."
         exit 2
     fi
 
-    docker run -t "${IMAGE_NAME}" ./gradlew test
+    docker run -t "${DOCKER_IMAGE_NAME}" ./gradlew test
 }
 
 
 function push {
-    docker history "${IMAGE_NAME}" 2> /dev/null
+    docker history "${DOCKER_IMAGE_NAME}" 2> /dev/null
 
     if [ $? -eq 1 ]; then
-        echo "Cant push ${IMAGE_NAME}, the image does not exist."
+        echo "Cant push ${DOCKER_IMAGE_NAME}, the image does not exist."
         exit 2
     fi
 
@@ -44,12 +42,17 @@ function push {
     [ -z "$DOCKER_PASSWORD" ] && { echo "Need to set DOCKER_PASSWORD"; exit 1; }
 
     if [[ $EUID -ne 0 ]]; then
+        if [[ -z $(which sudo) ]]; then
+            echo "Missing sudo client which is required for pushing when not root"
+            exit 2
+        fi
+
         sudo docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
-        sudo docker push "${IMAGE_NAME}"
+        sudo docker push "${DOCKER_IMAGE_NAME}"
 
     else
         docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
-        docker push "${IMAGE_NAME}"
+        docker push "${DOCKER_IMAGE_NAME}"
     fi
 
 }
@@ -68,14 +71,17 @@ fi
 for action in "${actions[@]}"; do
     case "$action" in
         build)
+            echo "Executing build action"
             build
             ;;
 
         test)
+            echo "Executing test action"
             test
             ;;
 
         push)
+            echo "Executing push action"
             push
             ;;
     esac
