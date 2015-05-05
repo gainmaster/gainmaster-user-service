c46acaffe860ca1458ff739371973fe46b9cad98
Knut Lorvik <lorviken@gmail.com>
2015-05-05 14:44:20 +0200

    Modify buildscript

--- build.sh
+++ build.sh
@@ -30,16 +30,20 @@ function test {
 
 
 function push {
-    docker history "${DOCKER_IMAGE_NAME}" 2> /dev/null
+    if [ -n "$BUILD_NUMBER" ]; then
+        "Not allowed to push a build without a build number!"
+    fi
+
+	docker history $DOCKER_IMAGE_NAME:${BUILD_NUMBER} 2> /dev/null
 
     if [ $? -eq 1 ]; then
-        echo "Cant push ${DOCKER_IMAGE_NAME}, the image does not exist."
+        echo "Cant push ${DOCKER_IMAGE_NAME}:${BUILD_NUMBER}, the image is not built"
         exit 2
     fi
 
-    [ -z "$DOCKER_EMAIL" ]    && { echo "Need to set DOCKER_EMAIL";    exit 1; }
-    [ -z "$DOCKER_USER" ]     && { echo "Need to set DOCKER_USER";     exit 1; }
-    [ -z "$DOCKER_PASSWORD" ] && { echo "Need to set DOCKER_PASSWORD"; exit 1; }
+    [ -z "$DOCKER_EMAIL" ]    && { echo "Need to set DOCKER_EMAIL";    exit 4; }
+    [ -z "$DOCKER_USER" ]     && { echo "Need to set DOCKER_USER";     exit 4; }
+    [ -z "$DOCKER_PASSWORD" ] && { echo "Need to set DOCKER_PASSWORD"; exit 4; }
 
     if [[ $EUID -ne 0 ]]; then
         if [[ -z $(which sudo) ]]; then
@@ -48,13 +52,17 @@ function push {
         fi
 
         sudo docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
-        sudo docker push "${DOCKER_IMAGE_NAME}"
-
+        if [ -z "$NOT_LATEST" ]; then
+            sudo docker push $DOCKER_IMAGE_NAME:latest
+        fi
+        sudo docker push $DOCKER_IMAGE_NAME:${BUILD_NUMBER}
     else
         docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
-        docker push "${DOCKER_IMAGE_NAME}"
+        if [ -z "$NOT_LATEST" ]; then
+            docker push $DOCKER_IMAGE_NAME:latest
+        fi
+        docker push $DOCKER_IMAGE_NAME:${BUILD_NUMBER}
     fi
-
 }
 
 
@@ -62,27 +70,52 @@ function push {
 # Handle input
 #
 
+#GETOPTS_STRING=`getopt -o b:l --long build-number: -n 'build.sh' -- "$@"`
+#eval set -- "$GETOPTS_STRING"
+
+while getopts "b:l --long build-number:" opt; do
+    case "$1" in
+        --build-number)
+            case "$2" in
+                "")  echo "Option --build-number expected a argument" ; exit 5 ;;
+                *) BUILD_NUMBER=$2; shift 2 ;;
+            esac ;;
+        --not-latest) NOT_LATEST=true ; shift ;;
+        --) shift ; break ;;
+        *) echo "Option $1 is not valid!"; exit 1 ;;
+    esac
+done
+
+if [ -z "$BUILD_NUMBER" ] && [ -n "$NOT_LATEST" ]; then
+    echo "A build can not have a build number and not be latest"
+    exit 5;
+fi
+
+shift $((OPTIND-1))
 actions=("$@")
 
 if [ ${#actions[@]} -eq 0 ]; then
-    actions=(build test push)
+    actions=(pre-build build post-build test push)
 fi
 
 for action in "${actions[@]}"; do
     case "$action" in
         build)
             echo "Executing build action"
-            build
+            build_application
+            build_docker_image
             ;;
-
         test)
             echo "Executing test action"
-            test
+            test_docker_image
             ;;
-
         push)
             echo "Executing push action"
-            push
+            push_docker_image
             ;;
+
+        --*) break ;;
+
+        *) echo "Ignoring invalid action ${action}" ;;
     esac
 done
