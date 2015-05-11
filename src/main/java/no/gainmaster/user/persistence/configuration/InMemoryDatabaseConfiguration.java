35b6e27d98c46f6ad08c9be34dbaf889ed42513f
Tony Hesjevik <tony@hesjevik.no>
2015-05-11 13:22:05 +0200

    Use gainmaster db

--- src/main/java/no/gainmaster/user/persistence/configuration/InMemoryDatabaseConfiguration.java
+++ src/main/java/no/gainmaster/user/persistence/configuration/InMemoryDatabaseConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.PropertyResolver;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -12,25 +11,24 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
-@Configuration
-@Profile("development")
-public class InMemoryDatabaseConfiguration implements EnvironmentAware {
-
-    private Environment environment;
-
-    private PropertyResolver propertyResolver;
-
-
-    @Override
-    public void setEnvironment(Environment environment) {
-        this.environment = environment;
-        this.propertyResolver = new RelaxedPropertyResolver(environment, "spring.datasource.");
-    }
-
-    @Bean
-    public DataSource embeddedDataSource() {
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        return builder.setType(EmbeddedDatabaseType.H2).build();
-    }
-
-}
+//@Configuration
+//public class InMemoryDatabaseConfiguration implements EnvironmentAware {
+//
+//    private Environment environment;
+//
+//    private PropertyResolver propertyResolver;
+//
+//
+//    @Override
+//    public void setEnvironment(Environment environment) {
+//        this.environment = environment;
+//        this.propertyResolver = new RelaxedPropertyResolver(environment, "spring.datasource.");
+//    }
+//
+//    @Bean
+//    public DataSource embeddedDataSource() {
+//        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+//        return builder.setType(EmbeddedDatabaseType.H2).build();
+//    }
+//
+//}
