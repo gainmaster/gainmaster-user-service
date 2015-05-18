addcc44d0a775917e42ca5cb7ec8c20334304bbe
Knut Lorvik <lorviken@gmail.com>
2015-05-18 12:12:35 +0200

    Allow options/post with or without trailing slash

--- src/main/java/no/gainmaster/user/web/config/OAuthConfiguration.java
+++ src/main/java/no/gainmaster/user/web/config/OAuthConfiguration.java
@@ -32,16 +32,15 @@ public class OAuthConfiguration extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        //Allow options pre-flight
-        http.authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/users/*").permitAll();
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.OPTIONS, "/users").permitAll()
+            .antMatchers(HttpMethod.OPTIONS, "/users/*").permitAll()
+            .antMatchers(HttpMethod.POST, "/users").permitAll()
+            .antMatchers(HttpMethod.POST, "/users/*").permitAll()
+            .anyRequest().authenticated();
 
-        //Allow POST user if client has admin authority
-        //TODO: Remove access for all
-        http.authorizeRequests().antMatchers(HttpMethod.POST, "/users").permitAll();
+        //TODO: Remove access for all and allow only admins to post
         //http.authorizeRequests().antMatchers(HttpMethod.POST, "/users").hasAuthority("ROLE_ADMIN");
-
-        //Require authentication on all resources
-        http.authorizeRequests().anyRequest().authenticated();
     }
 
     @Bean
